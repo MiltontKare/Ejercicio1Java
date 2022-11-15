0d45ec9966d2cf4ecd36313320ad331fe881e5c8
Miltont Toalombo Goyes <45127349+xXMiltonTGXx@users.noreply.github.com>
2022-11-15 01:03:44 -0500

    Add files via upload

---
+++ CURSO JAVASCRIPT/Ejercicio 4.js
@@ -0,0 +1,12 @@
+let altura_cm = 160;
+let altura_m = 1.6;
+let peso_kg = 90.8;
+
+console.log(altura_m.toFixed(0));
+let altura_red = Math.ceil(altura_m);
+console.log(altura_red);
+
+console.log(parseInt(peso_kg));
+let peso_red = Math.floor(peso_kg);
+console.log(peso_red);
+let sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
