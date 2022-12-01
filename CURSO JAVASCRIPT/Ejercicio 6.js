f1888ee2b0368629d97747e95016f9ffa6f835c6
Miltont Toalombo Goyes <45127349+xXMiltonTGXx@users.noreply.github.com>
2022-12-01 14:28:48 -0600

    Add files via upload

---
+++ CURSO JAVASCRIPT/Ejercicio 6.js
@@ -0,0 +1,13 @@
+/* */
+const miSet = new Set();
+
+miSet.add("Milton");
+miSet.add("Ana");
+miSet.add("Fanit");
+console.log(miSet);
+miSet.add("Milton");
+
+console.log(miSet);
+miSet.add("Javascript");
+
+console.log(miSet);
