456b597e0fff4765db7d117cc47305a23a884d73
Miltont Toalombo Goyes <45127349+xXMiltonTGXx@users.noreply.github.com>
2022-12-08 20:29:35 -0600

    Add files via upload

---
+++ CURSO JAVASCRIPT/Ejercicio 7.js
@@ -0,0 +1,16 @@
+function devuelveTrue() {
+  return true;
+}
+
+async function damePromesa() {
+  return setTimeout(() => console.log("Hola soy una promesa"), 5000);
+}
+
+damePromesa();
+
+function* idsPares() {
+  let id = 0;
+  while (true) {
+    yield (id += 2);
+  }
+}
